1bb231f60980ed0477c73453ac7d9ed831783ad2
danielmb795 <danielmachadoborges795@gmail.com>
2024-12-26 14:49:11 -0300

    Add files via upload

---
+++ Servidor de Arquivos/go.mod
@@ -0,0 +1,9 @@
+module servidor
+
+go 1.23.3
+
+require (
+	github.com/abbot/go-http-auth v0.4.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+)
---
+++ Servidor de Arquivos/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	auth "github.com/abbot/go-http-auth"
+)
+
+func Secret(user, realm string) string{
+	if user == "user"{
+		return "$1$NJb3xbck$S0pO/8BdnoeW1pnn3EAwc1"
+	}
+	return ""
+	
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Uso: go run <Caminho Diretório> <porta>")
+	}
+	httpdir := os.Args[1]
+	porta := os.Args[2]
+	http.FileServer(http.Dir(httpdir))
+
+	authenticator := auth.NewBasicAuthenticator("server.com", Secret )
+	http.HandleFunc("/", authenticator.Wrap(func (w http.ResponseWriter, r *auth.AuthenticatedRequest){
+		http.FileServer(http.Dir(httpdir)).ServeHTTP(w, &r.Request)
+	}))
+
+
+	log.Println("Servidor rodando na porta", porta)
+	log.Fatal(http.ListenAndServe(":"+porta, nil))
+}
